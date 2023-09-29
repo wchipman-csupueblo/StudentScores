5af8091249122c6ad016234d2ae58b865100dee1
bill chipman <wrchipman@gmail.com>
2023-09-28 22:50:57 -0400

    Student Scores design completed

--- StudentScores/frmStudentScores.Designer.cs
+++ StudentScores/frmStudentScores.Designer.cs
@@ -29,22 +29,158 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.lstStudents = new System.Windows.Forms.ListBox();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblScoreTotal = new System.Windows.Forms.Label();
+            this.lblScoreCount = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(53, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Students";
+            // 
+            // lstStudents
+            // 
+            this.lstStudents.FormattingEnabled = true;
+            this.lstStudents.ItemHeight = 15;
+            this.lstStudents.Location = new System.Drawing.Point(16, 31);
+            this.lstStudents.Margin = new System.Windows.Forms.Padding(2);
+            this.lstStudents.Name = "lstStudents";
+            this.lstStudents.Size = new System.Drawing.Size(229, 79);
+            this.lstStudents.TabIndex = 1;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(257, 31);
+            this.btnAddNew.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 2;
+            this.btnAddNew.Text = "&Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(257, 58);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "&Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(257, 86);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "&Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(257, 167);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 120);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Score total:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblScoreTotal
+            // 
+            this.lblScoreTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblScoreTotal.Location = new System.Drawing.Point(201, 116);
+            this.lblScoreTotal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblScoreTotal.Name = "lblScoreTotal";
+            this.lblScoreTotal.Size = new System.Drawing.Size(44, 23);
+            this.lblScoreTotal.TabIndex = 7;
+            this.lblScoreTotal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblScoreCount
+            // 
+            this.lblScoreCount.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblScoreCount.Location = new System.Drawing.Point(201, 141);
+            this.lblScoreCount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblScoreCount.Name = "lblScoreCount";
+            this.lblScoreCount.Size = new System.Drawing.Size(44, 23);
+            this.lblScoreCount.TabIndex = 9;
+            this.lblScoreCount.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(124, 145);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Score count:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAverage.Location = new System.Drawing.Point(201, 167);
+            this.lblAverage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(44, 23);
+            this.lblAverage.TabIndex = 11;
+            this.lblAverage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(144, 171);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Average:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // frmStudentScores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(351, 208);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblScoreCount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblScoreTotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.lstStudents);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -58,6 +194,17 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstStudents;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblScoreTotal;
+        private System.Windows.Forms.Label lblScoreCount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label label6;
     }
 }
 
